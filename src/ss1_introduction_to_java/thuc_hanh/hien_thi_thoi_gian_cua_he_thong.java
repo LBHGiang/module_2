03b6caab4ffe441256564dd498c8b3d50c3261a8
unknown <hgno3001@gmail.com>
2022-08-01 16:31:28 +0700

    ss1

---
+++ src/ss1_introduction_to_java/thuc_hanh/hien_thi_thoi_gian_cua_he_thong.java
@@ -0,0 +1,11 @@
+package ss1_introduction_to_java.thuc_hanh;
+
+import java.util.Date;
+
+public class hien_thi_thoi_gian_cua_he_thong {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is: " + now);
+    }
+
+}
