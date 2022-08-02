114793a4c6110a9a5028e0a7f78ce75b45e8105c
unknown <hgno3001@gmail.com>
2022-08-02 21:00:13 +0700

    up exercise ss3 array

---
+++ src/ss3_arrays_and_methods_in_java/exercise/AddElementToArray.java
@@ -0,0 +1,20 @@
+package ss3_arrays_and_methods_in_java.exercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AddElementToArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = {1, 2, 3, 4, 3, 6, 7, 3, 1};
+
+        System.out.print("Enter the element to remove ");
+        int index = Integer.parseInt(scanner.nextLine());
+
+        for (int i = array.length - 1; i > index; i--) {
+            array[i] = array[i - 1];
+        }
+        array[index] = k;
+        System.out.println(Arrays.toString(array));
+    }
+}
