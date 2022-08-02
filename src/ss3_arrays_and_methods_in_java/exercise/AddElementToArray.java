4d066364813d4fef0f2432774e300cb6bfe4de03
unknown <hgno3001@gmail.com>
2022-08-02 21:04:35 +0700

    update ss3 exercise2

--- src/ss3_arrays_and_methods_in_java/exercise/AddElementToArray.java
+++ src/ss3_arrays_and_methods_in_java/exercise/AddElementToArray.java
@@ -8,13 +8,16 @@ public class AddElementToArray {
         Scanner scanner = new Scanner(System.in);
         int[] array = {1, 2, 3, 4, 3, 6, 7, 3, 1};
 
-        System.out.print("Enter the element to remove ");
+        System.out.print("Enter the element to add ");
+        int element = Integer.parseInt(scanner.nextLine());
+
+        System.out.print("Enter the index to add ");
         int index = Integer.parseInt(scanner.nextLine());
 
         for (int i = array.length - 1; i > index; i--) {
             array[i] = array[i - 1];
         }
-        array[index] = k;
+        array[index] = element;
         System.out.println(Arrays.toString(array));
     }
 }
