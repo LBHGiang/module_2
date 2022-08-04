8c1a06ccae17d177d9ed25f086820c2d4fc9e306
unknown <hgno3001@gmail.com>
2022-08-04 20:53:48 +0700

    up ss4 exercise

---
+++ src/ss4_class_object/exercise/fan/Fan.java
@@ -0,0 +1,87 @@
+package ss4_class_object.exercise.fan;
+
+public class Fan {
+
+    int speed = 1;
+    private boolean on = false;
+    private double radius = 5;
+    private String color = "blue";
+
+    public void setSpeed(int speed) {
+        if (speed != 1 || speed != 2 || speed != 3) {
+            System.out.println("can't set " + speed + "! Speed must be 1,2 or 3.");
+        } else {
+        this.speed = speed;}
+    }
+
+    public void setOn() {
+        this.on = true;
+    }
+
+    public void setOff() {
+        this.on = false;
+    }
+
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSpeed() {
+        if (!on) {
+            return "Fan is off";
+        } else {
+            switch (this.speed) {
+                case 1:
+                    return "SLOW";
+                case 2:
+                    return "MEDIUM";
+                case 3:
+                    return "FAST ";
+                default:
+                    return "Fan is broken";
+            }
+        }
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    //constructor
+
+    public Fan() {
+        this.speed = 1;
+        this.on = false;
+        this.radius = 5;
+        this.color = "blue";
+    }
+
+    @Override
+    public String toString() {
+        if (on) {
+            return "Fan is on {" +
+                    "speed=" + getSpeed() +
+                    ", radius=" + radius +
+                    ", color='" + color + '\'' +
+                    '}';
+        } else {
+            return "Fan is off {" +
+                    ", radius=" + radius +
+                    ", color='" + color + '\'' +
+                    '}';
+        }
+    }
+}
--- src/ss4_class_object/exercise/stop_watch/MainController.java
+++ src/ss4_class_object/exercise/stop_watch/MainController.java
@@ -4,22 +4,20 @@ import java.util.Arrays;
 
 public class MainController {
     public static void main(String[] args) {
-        int[] array = {1,35,7,4,12,76,12,54,6756,234,123,564,34,234,2634,53425,436};
+        int[] array = new int[10000];
         StopWatch stopWatch1 = new StopWatch();
 
         //start
         System.out.println(stopWatch1.start());
 
-        //sort array
-        for(int i=0;i<array.length-1;i++){
-            for(int j=i+1;j<array.length;j++){
-                if(array[i] < array[j]){
-                    int x = array[i];
-                    array[i] = array[j];
-                    array[j] = x;
-                }
-            }
+        //input element
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) Math.floor(Math.random() * 100000 + 1);
         }
+
+        //sort array
+        Arrays.sort(array);
+
         //display array
         System.out.println(Arrays.toString(array));
 
